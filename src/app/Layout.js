de9b0d54ba7184cf3aaafa872ee4e8149fb8d305
Tanner Stephens <tannerrstephens@gmail.com>
2019-03-05 02:23:27 -0600

    Some more initial setup - bulma, sass, etc

---
+++ src/app/Layout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Button from 'react-bulma-components/lib/components/button';
+
+class Layout extends Component {
+  render() { 
+    return ( 
+      <Button color="primary">Test Button</Button>
+    );
+  }
+}
+ 
+export default Layout;
